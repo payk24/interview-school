16f244f79b4453dbd4a15eb9fa74d3413fd59003
Ihor Boiko <payk.ace@gmail.com>
2025-01-04 22:43:02 +0200

    add Student

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :students
   resources :classrooms
   resources :subjects
   resources :teachers
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_04_204042) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_04_204231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "classrooms", force: :cascade do |t|
     t.integer "number", null: false
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
