2a7ffaffdcc2a92ce3a369156a084d1e1bc96eb2
Ihor Boiko <payk.ace@gmail.com>
2025-01-04 22:38:23 +0200

    add Subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_04_203241) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_04_203732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "first_name", null: false
